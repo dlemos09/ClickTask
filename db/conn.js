4c8af336d44782d019d25da2f9b124a3042870fb
Douglas Lemos <dlfrontend09@gmail.com>
2024-12-10 22:59:57 -0300

    update

--- db/conn.js
+++ db/conn.js
@@ -1,22 +1,35 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-    }
-);
+require('dotenv').config();
+ 
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 
-// Testando a conexão com o banco de dados
-try {
-    sequelize.authenticate();
-    console.log('Conectamos com sucesso ao Sequelize');
-} catch (err) {
-    console.log('Não foi possível conectar: ', err);
-}
+    host: process.env.DB_HOST,
+
+    port: process.env.DB_PORT,
+
+    dialect: process.env.DB_DIALECT,
+
+    dialectOptions: {
+
+        ssl: {
+
+            require: true,
+
+            rejectUnauthorized: false // Dependendo da configuração de SSL
+
+        }
+
+    }
+
+});
+ 
+sequelize.authenticate()
+
+    .then(() => console.log('Conexão bem-sucedida com o banco de dados!'))
+
+    .catch(err => console.error('Erro ao conectar:', err));
+
+ 
 
 module.exports = sequelize;
